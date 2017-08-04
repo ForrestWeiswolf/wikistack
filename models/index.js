5f70f91f38cf284d5efe5849d40a147991c37b61
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-08-04 17:29:25 -0400

    Attempt to create Page and User tables

--- models/index.js
+++ models/index.js
@@ -1,2 +1,18 @@
 var Sequelize = require('sequelize');
 var db = new Sequelize('postgres://localhost:5432/wikistack');
+
+db.sync(function(err) {
+  if err throw err
+
+  db.define('Page', {
+    title: Sequelize.STRING,
+    urlTitle: Sequelize.STRING,
+    content: Sequelize.TEXT,
+    status: Sequelize.BOOLEAN
+  });
+
+  db.define('User', {
+    name: Sequelize.STRING,
+    email: {type: Sequelize.STRING, unique: true}
+  });
+});
