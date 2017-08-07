109198c75e2bea782d303c9f7eeeaf016605658b
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-08-07 11:03:22 -0400

    add validation

--- models/index.js
+++ models/index.js
@@ -2,27 +2,16 @@ var Sequelize = require('sequelize');
 var db = new Sequelize('postgres://localhost:5432/wikistack');
 
 var Page = db.define('page', {
-    title: {
-        type: Sequelize.STRING
-    },
-    urlTitle: {
-        type: Sequelize.STRING
-    },
-    content: {
-        type: Sequelize.TEXT
-    },
-    status: {
-        type: Sequelize.ENUM('open', 'closed')
-    }
+    title: { type: Sequelize.STRING, allowNull: false },
+    urlTitle: { type: Sequelize.STRING, allowNull: false },
+    content: { type: Sequelize.STRING, allowNull: false },
+    status: { type: Sequelize.ENUM('open', 'closed'), defaultValue: 'open'},
+    date: { type: Sequelize.DATE, defaultValue: Sequelize.NOW }
 });
 
 var User = db.define('user', {
-    name: {
-        type: Sequelize.STRING
-    },
-    email: {
-        type: Sequelize.STRING
-    }
+    name: { type: Sequelize.STRING, allowNull: false },
+    email: { type: Sequelize.STRING, allowNull: false, validate: { isEmail: true } }
 });
 
 module.exports = db
