aeb138acab5e355b9710977c02d8bfa5fd024e10
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-08-07 10:47:31 -0400

    Use provided code to export schames instead of syncing

--- models/index.js
+++ models/index.js
@@ -1,18 +1,31 @@
 var Sequelize = require('sequelize');
 var db = new Sequelize('postgres://localhost:5432/wikistack');
 
-db.sync(function(err) {
-  if err throw err
-
-  db.define('Page', {
-    title: Sequelize.STRING,
-    urlTitle: Sequelize.STRING,
-    content: Sequelize.TEXT,
-    status: Sequelize.BOOLEAN
-  });
-
-  db.define('User', {
-    name: Sequelize.STRING,
-    email: {type: Sequelize.STRING, unique: true}
-  });
+var Page = db.define('page', {
+    title: {
+        type: Sequelize.STRING
+    },
+    urlTitle: {
+        type: Sequelize.STRING
+    },
+    content: {
+        type: Sequelize.TEXT
+    },
+    status: {
+        type: Sequelize.ENUM('open', 'closed')
+    }
 });
+
+var User = db.define('user', {
+    name: {
+        type: Sequelize.STRING
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+});
+
+module.exports = {
+  Page: Page,
+  User: User
+};
